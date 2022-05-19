2cbdc8e3e53b5ced606940ed47e7d09656726ebb
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-05-19 07:03:06 -0400

    completing 9.2.7

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 /*
  - Create a variable that should NOT have its value reasigned --> const profileDataArg
  - Equal this variable to "process.argv" --> This property returns an array containing the command-line arguments passed when the Node.js process was launched.
@@ -6,38 +7,19 @@
     * .slice(2) --> returns a new array based on process.argv astarting a the THIRD index (2)
     * process.argv.length --> iteration as per the process.argv
  */
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
 const fs = require('fs');
-
-console.log(profileDataArgs);
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+//inporting from the page-template.js the html frame
+const generatePage = require('./src/page-template.js');
 
 /*
 - accessint the file system
 - using the writeFile functionality
 - 3 arguments inside: 1- name of the file, 2- the data that's beeing written, 3- callback for errors
  */
-FileSystem.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output');
 });
