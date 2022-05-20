900cae814861e5cd18e1ced6e0a3af143d04f20b
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-05-20 19:22:58 -0400

    getting an error after completing all steps in this module section

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //user info
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 const promptUser = () => {
   return inquirer.prompt([
@@ -132,5 +135,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
