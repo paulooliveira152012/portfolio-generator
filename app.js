00d8994fda9733f1eafe148ff86f2d7e678046c1
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-05-19 21:03:19 -0400

    validating answers

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username',
+      validate: nameInput => {
+        if (nameInput){
+          return true;
+        } else {
+          console.log('Please enter your Github username')
+        }
+      }
     },
     {
       type: 'confirm',
@@ -43,12 +58,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter the name of your project');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true
+        } else {
+          console.log('Provde a description of your project')
+        }
+      }
+      
     },
     {
       type: 'checkbox',
@@ -59,7 +90,14 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true
+        } else {
+          console.log('Enter the loink to your Github account')
+        }
+      }
     },
     {
       type: 'confirm',
