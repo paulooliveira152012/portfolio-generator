0657d6dcb354109571c03cdb71a4dd9874ab9732
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-05-24 18:14:13 -0400

    done with 9.5.7

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -22,7 +22,7 @@ const writeFile = fileContent => {
 
   const copyFile = () => {
       return new Promise((resolve, reject) => {
-          fs.copyFile('./dist/index.html', fileContent, err => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
             if (err) {
                 reject(err);
                 return
